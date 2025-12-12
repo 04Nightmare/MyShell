88e4701980a0d7482ef496620faa0e23799a1ec7
Samannyo Pal <samannyopal04@gmail.com>
2025-12-13 04:23:32 +0530

    run executable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::Command;
 use pathsearch::find_executable_in_path;
 
 fn main() {
@@ -10,11 +11,12 @@ fn main() {
     
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        if command.trim() == "exit" {
+        let trimmed_command = command.trim();
+        if trimmed_command == "exit" {
             break;
-        }else if command.trim().starts_with("echo") {
+        }else if trimmed_command.starts_with("echo") {
             println!("{}",command.split_off(5).trim());
-        }else if command.trim().starts_with("type"){
+        }else if trimmed_command.starts_with("type"){
             let arg = command.split_off(5).trim().to_string();
             if builtin_commands.contains(&arg.as_str()){
                 println!("{} is a shell builtin", arg);
@@ -23,8 +25,22 @@ fn main() {
             }else{
                 println!("{}: not found", arg);
             }
-        }else{
-            println!("{}: command not found", command.trim());
+        }else {
+            let command_split: Vec<&str> = trimmed_command.split_whitespace().collect();
+            if command_split.is_empty(){
+                println!("{}: command not", trimmed_command);
+            }else{
+                let dynamic_args = &command_split[1..];
+                let output = Command::new(command_split[0])
+                    .args(dynamic_args)
+                    .output();
+
+                match output {
+                    Ok(output) => {println!("{:?}",output.stdout.as_slice());}
+                    Err(_) => {println!("{}: command not found", trimmed_command)}
+                }
+                
+            }
         }
     }
 }
