719e2ac33b14eb56df73eed4c94df4457d9aa1dc
Samannyo Pal <samannyopal04@gmail.com>
2025-12-13 06:14:08 +0530

    fixed problems

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,17 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
 
         let shell_command: Vec<&str> = input.trim().split_whitespace().collect();
-        let args = &shell_command[1..];
-
-        match shell_command[0]{
-            "exit" => break,
-            "echo" => echo_command(args),
-            "type" => type_command(args),
-            _ => check_command(&input),
+        if shell_command.is_empty(){
+            println!("{}: command not found", input.trim());
+        }else{
+            let args = &shell_command[1..];
+    
+            match shell_command[0]{
+                "exit" => break,
+                "echo" => echo_command(args),
+                "type" => type_command(args),
+                _ => check_command(&input),
+            }
         }
     }
 }
@@ -32,28 +36,31 @@ fn echo_command(args: &[&str]){
 fn type_command(args: &[&str]){
     if !args.is_empty() && BUILTIN_COMMANDS.contains(&args[0]){
         println!("{} is a shell builtin", args[0]);
+        return;
     }else if !args.is_empty(){
         if let Some(path) = find_executable_in_path(&args[0]){
             println!("{} is {}", args[0], path.display());
+            return;
         }
-    }else{
-        println!("{}: not found", args[0]);
     }
+    println!("{}: not found", args[0]);
 }
 
 
 fn check_command(cmd: &str){
     let command_split: Vec<&str> = cmd.split_whitespace().collect();
-    let dynamic_args = &command_split[1..];
-    if command_split.is_empty(){
-        println!("{}: command not found", cmd);
-    }else{
-        let mut output = Command::new(command_split[0])
-            .args(dynamic_args)
-            .spawn()
-            .unwrap();
-        output.wait().unwrap();
-        return;
+    if command_split.len() > 1{
+        let dynamic_args = &command_split[1..];
+        if command_split.is_empty(){
+            println!("{}: command not found", cmd);
+        }else{
+            let mut output = Command::new(command_split[0])
+                .args(dynamic_args)
+                .spawn()
+                .unwrap();
+            output.wait().unwrap();
+            return;
+        }
     }
     println!("{}: command not found", &command_split[0])
 }
